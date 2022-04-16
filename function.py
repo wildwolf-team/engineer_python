abe0be1bb063481ec4062df0cd6c07cbf15c1b78
Gu-nian <921690547@qq.com>
2022-04-16 20:34:47 +0800

    基数排序

--- function.py
+++ function.py
@@ -14,6 +14,7 @@ from utils.general import check_img_size,non_max_suppression,scale_coords, xyxy2
 from utils.torch_utils import select_device
 import serial
 import time
+from typing import List
 
 class Function:
 
@@ -21,7 +22,7 @@ class Function:
     DIRECTION = -1
     SEND_DATA_1 = -1
     SEND_DATA_0 = -1
-
+    TARGET_X = 400
     def __init__(self,weights):
         self.ser = serial.Serial()
         self.ser.port = "/dev/ttyUSB0"
@@ -43,6 +44,18 @@ class Function:
         self.imgsz = check_img_size((320,320),s=self.stride)
         self.model.model.float()
     
+    def radix_sort(arr:List[int]):
+        n = len(str(max(arr)))  # 记录最大值的位数
+        for k in range(n):#n轮排序
+            # 每一轮生成10个列表
+            bucket_list=[[] for i in range(10)]#因为每一位数字都是0~9，故建立10个桶
+            for i in arr:
+                # 按第k位放入到桶中
+                bucket_list[i//(10**k)%10].append(i)
+            # 按当前桶的顺序重排列表
+            arr=[j for i in bucket_list for j in i]
+        return arr
+
     def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
         # Resize and pad image while meeting stride-multiple constraints
         
@@ -76,7 +89,6 @@ class Function:
         im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
         return im, ratio, (dw, dh)
 
-
     # 进行推理 绘制图像 结算出最优 发送数据
     def to_inference(self, frame, device, model, imgsz, stride, conf_thres=0.45, iou_thres=0.45):
         img_size = frame.shape
@@ -94,12 +106,10 @@ class Function:
         pred = model(img)
         pred = non_max_suppression(pred, conf_thres, iou_thres, agnostic=False)
         aims = []
-        min = 10000
-        num = 0
-        model = false
         self.direction = 0
         self.deviation_x = 0
         confs = []
+        arr = []
 
         for i ,det in enumerate(pred):
             gn = torch.tensor(img0.shape)[[1,0,1,0]]
@@ -117,7 +127,6 @@ class Function:
 
             if len(aims):
                 for i,det in enumerate(aims):
-                    
                     tag, x_center, y_center, width, height = det
                     x_center, width = float(x_center) * img_size[1], float(width) * img_size[1]
                     y_center, height = float(y_center) * img_size[0], float(height) * img_size[0]
@@ -129,32 +138,21 @@ class Function:
                     cv2.putText(frame,str(float(round(confs[i], 2))), top_right, cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 4)
                     cv2.putText(frame, tag, top_left, cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 255), 4)
 
-                    # box_distance = math.sqrt(pow((top_left[0]+bottom_right[0] - 400), 2) + pow((top_left[1]+bottom_right[1] - img_size[0]), 2))
-                    box_distance = abs(x_center - 400)
-                    if box_distance < min:
-                        min = box_distance
-                        num = i
-
-                last_box = aims[num]
-                tag, x_center, y_center, width, height = last_box
-                x_center, width = float(x_center) * img_size[1], float(width) * img_size[1]
-                y_center, height = float(y_center) * img_size[0], float(height) * img_size[0]
-
-                top_left = (int(x_center - width * 0.5), int(y_center - height * 0.5))
-                bottom_right = (int(x_center + width * 0.5), int(y_center + height * 0.5))
-                self.deviation_x = int(x_center - 400 )
-                self.deviation_y = int(y_center - int(img_size[0]/2))
-
+                    arr.append(int(x_center - Function.TARGET_X))
+               
+                if abs(Function.radix_sort(arr)[0]) < abs(Function.radix_sort(arr)[len(arr)-1]):
+                    self.deviation_x = Function.radix_sort(arr)[0]
+                else:
+                    self.deviation_x = Function.radix_sort(arr)[len(arr)-1]
+                cv2.putText(frame, "x = " + str(self.deviation_x), (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 4)
                 if abs(self.deviation_x) < (bottom_right[0]- top_left[0] - 10)/4:
                     self.deviation_x = 0
                 if self.deviation_x > 0:
                     self.direction = 1
-                model = true
-                cv2.putText(frame, "x = " + str(self.deviation_x), (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 4)
-                cv2.putText(frame, "y = " + str(self.deviation_y), (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 4)
+                cv2.putText(frame, "x = " + str(self.deviation_x), (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 4)
 
             cv2.line(frame, (0, int(img_size[0] * 0.5)), (int(img_size[1]), int(img_size[0] * 0.5)), (255, 0, 255), 3)
-            cv2.line(frame, (400, 0), (400, int(img_size[0])), (255, 0, 255), 3)
+            cv2.line(frame, (Function.TARGET_X, 0), (Function.TARGET_X, int(img_size[0])), (255, 0, 255), 3)
 
             self.send_data_0 = (self.deviation_x >> 8) & 0xff
             self.send_data_1 = self.deviation_x & 0xff
