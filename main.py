2c35affb2cc1d9bdbf5a4ede7f1622734481c4c7
Gu-nian <921690547@qq.com>
2022-05-28 17:19:24 +0800

    完整版+

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ import threading
 def parse_opt():
     parser = argparse.ArgumentParser()
     # 自启动 default 要改成绝对路径
-    parser.add_argument('--weights', nargs='+', type=str, default='/home/wolfvision-nuc01/python_thread/best (3).pt', help='model path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default='/home/oyc/workspace/python_thread/best (3).pt', help='model path(s)')
     opt = parser.parse_args()
     return opt
 
-def run(Video, Fun, mode = 0):
+def run(Video, Fun, is_save = 0, mode = 0):
     
     while (cv2.waitKey(1) & 0xFF) != ord('q'):
         try:
@@ -28,7 +28,7 @@ def run(Video, Fun, mode = 0):
             frame = frame.reshape((FrameHead.iHeight, FrameHead.iWidth, 1 if FrameHead.uiMediaType == mvsdk.CAMERA_MEDIA_TYPE_MONO8 else 3) )
             
             Fun.to_inference(frame, Fun.device, Fun.model, Fun.imgsz, Fun.stride, mode=mode)
-
+            
             t3 = time_sync()
 
             if Video.IS_SAVE_VIDEO:
@@ -37,10 +37,11 @@ def run(Video, Fun, mode = 0):
                 except:
                     print("Write Frame Error")
             cv2.imshow("frame",frame)
-            # print("Inference == " + str(1/(t3 - t2)))
+            print("Inference == " + str(1/(t3 - t2)))
         except mvsdk.CameraException as e:
             if e.error_code != mvsdk.CAMERA_STATUS_TIME_OUT:
                 print("CameraGetImageBuffer failed({}): {}".format(e.error_code, e.message) )
+            video_capture.Video_capture.__init__(is_save)
     if Video.IS_SAVE_VIDEO:
         try:
             Video.out.release()
@@ -52,25 +53,30 @@ def run(Video, Fun, mode = 0):
     # 释放帧缓存
     mvsdk.CameraAlignFree(Video.pFrameBuffer)
     
-'''
-video 
-    0 为不录制视频
-    1 为录制视频
-    默认 0
-run
-    0 为原图
-    1 为putext后的图
-    默认 1
-'''
+
 if __name__ == "__main__":
     opt = parse_opt()
-    # 0 不储存图像
-    # 1 储存图像
-    Video = video_capture.Video_capture(1)
+    '''
+    is_save
+        0 不储存图像
+        1 储存图像
+        默认 0
+    '''
+    '''
+    mode
+        0 原图
+        1 处理图
+        默认 1
+    '''
+    is_save = 0
+    mode = 0
+    Video = video_capture.Video_capture(is_save)
     Fun = function.Function(**vars(opt))
-
-    thread1 = threading.Thread(target=(Fun.receive_data),daemon=True)
-    thread2 = threading.Thread(target=(Fun.send_data),daemon=True)
+    
+    thread1 = threading.Thread(target=(Fun.receive_data))
+    thread2 = threading.Thread(target=(Fun.send_data))
     thread1.start()
     thread2.start()
-    run(Video,Fun,1)
+    
+    run(Video,Fun,is_save,mode)
+
